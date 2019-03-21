cd38deadf15b36dec238b417960f7d60c573e699
matrixyz <matrixyz@163.com>
2019-03-21 11:34:42 +0800

    正式发布版1.0，没有SSL功能

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -70,7 +70,7 @@ public class Application {
      * @return
      */
     //@Bean
-    public org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory servletContainer() {
+   /* public org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory servletContainer() {
         org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory tomcat = new org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory() {
             @Override
             protected void postProcessContext(Context context) {
@@ -84,9 +84,9 @@ public class Application {
         };
         tomcat.addAdditionalTomcatConnectors(httpConnector());
         return tomcat;
-    }
+    }*/
     //@Bean
-    public Connector httpConnector() {
+ /*   public Connector httpConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
         //Connector监听的http的端口号
@@ -95,6 +95,6 @@ public class Application {
         //监听到http的端口号后转向到的https的端口号
         connector.setRedirectPort(443);
         return connector;
-    }
+    }*/
 
 }
